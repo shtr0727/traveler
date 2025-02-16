38f6e980d00647b898c8374b4d1a6f620b7aa5dd
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-02-16 18:10:45 +0900

    投稿一覧ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,13 @@ class PostsController < ApplicationController
     redirect_to '/top'
   end
 
+  def index
+    @posts = Post.all
+  end
+
   private
   def post_params
     params.require(:post).permit(:location, :text)
   end
-  
+
 end
