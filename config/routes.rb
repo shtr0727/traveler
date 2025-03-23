026b0f4eddaf04d2e6b757d811d8a95ff75885b1
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-03-23 17:10:47 +0900

    マイページ編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :users, only:[:index, :show]
+  resources :users, only:[:index, :show, :edit, :update]
 
   resources :posts do
     resources :comments, only:[:create, :destroy]
