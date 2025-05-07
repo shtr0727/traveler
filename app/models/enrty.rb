66f71ccc2e58abdaf6b1d6ea55fb01f416514466
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-05-07 23:26:05 +0900

    [update]DM機能用アソシエーションの追加

--- app/models/enrty.rb
+++
@@ -1,4 +0,0 @@
-class Enrty < ApplicationRecord
-  belongs_to :user
-  belongs_to :room
-end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many: entries: :destroy
+  has_many: messages: :destroy
 end
