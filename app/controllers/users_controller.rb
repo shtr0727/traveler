6c5bd32abdbef563663fc5d0a90273ecfdb56ee1
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-05-11 21:46:35 +0900

    [update]相互フォロー判定とDM部屋作成の記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,26 @@ class UsersController < ApplicationController
     @posts = @user.posts.page(params[:page]).per(8).reverse_order
     @following_users = @user.following_user
     @follower_users = @user.follower_user
+
+    # 現在ログインしているユーザー（自分）をEntriseテーブルから取得する。
+    @current_entry = Entry.where(user_id: current_user.id)
+    # メッセージ相手になるユーザーEntriseテーブルから取得する。
+    @another_entry = Entry.where(user_id: @user.id)
+    # 相手が自分自身ではなく、相互フォローしている場合のみDM処理を許可。
+    if @user.id != current_user.id && current_user.mutual_follow?(@user)
+      @current_entry.each do |current|
+        @another_entry.each do |another|
+          if current.room_id == another.room_id # each doで取り出されたcurrentとanotherが一致する場合は
+            @is_room = true # 自分と相手が同じroomに存在しているフラグ
+            @room_id = current.room_id # 現在のroom idをインスタンス変数として扱いビューに渡す
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def edit
