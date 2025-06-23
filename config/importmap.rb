6431f8232d26ae7c06615fef78e8e1d14c399e04
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-06-23 23:25:48 +0900

    [add]アセット読み込み（importmap）

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "jquery" # @3.7.1
+pin "moment" # @2.30.1
+pin "bootstrap-daterangepicker" # @3.1.0
